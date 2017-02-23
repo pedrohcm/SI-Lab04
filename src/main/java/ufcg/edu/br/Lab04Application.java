d6e36e125faff886935759e7a708028a67038571
pedrohcm <pedro.maia@ccc.ufcg.edu.br>
2017-02-23 09:57:03 -0300

    Finalizando...

--- src/main/java/ufcg/edu/br/Lab04Application.java
+++ src/main/java/ufcg/edu/br/Lab04Application.java
@@ -2,11 +2,29 @@ package ufcg.edu.br;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Lab04Application {
 
+	private static final String[] REQUEST_METHOD_SUPPORTED = {"GET", "POST", "PUT", "DELETE"};
+
 	public static void main(String[] args) {
 		SpringApplication.run(Lab04Application.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*")
+						.allowedMethods(REQUEST_METHOD_SUPPORTED);
+			}
+		};
 	}
 }
